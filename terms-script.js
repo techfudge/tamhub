efef63c9d510b0fc7e54148604527ea7b7fe5f73
Biggs <personal@My-Macbook-Pro.local>
2023-12-04 11:17:50 +0100

    second commit

--- terms-script.js
+++ terms-script.js
@@ -1,34 +1,12 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var form = document.getElementById("signupForm");
-  
-    if (form) {
-      form.addEventListener("submit", function(event) {
-        event.preventDefault(); // Prevents the default form submission
-  
-        var nameInput = document.getElementById("name");
-        var emailInput = document.getElementById("email");
-  
-        if (nameInput && emailInput) {
-          var name = nameInput.value.trim();
-          var email = emailInput.value.trim();
-  
-          // Perform validation - Example: check if fields are not empty
-          if (name !== '' && email !== '') {
-            // Here, you can perform further validation if needed
-            
-            // For simplicity, assuming the data is valid and proceed to redirect
-            window.location.href = "thankyou.html";
-          } else {
-            // Handle validation errors (e.g., show an error message)
-            // Example: alert("Please fill in all fields.");
-            console.error("Please fill in all fields.");
-          }
-        } else {
-          console.error("Form elements not found.");
-        }
-      });
-    } else {
-      console.error("Form not found.");
-    }
-  });
-  
+// Function for declining
+document.querySelector('.btn.gray-btn').addEventListener('click', function() {
+  alert("Registration declined. Sorry to see you go!");
+  // You can also redirect to another page or perform other actions as needed
+  window.close(); // Terminate the page
+});
+
+// Function for accepting
+document.querySelector('.btn.red-btn').addEventListener('click', function() {
+  alert("Thank you for accepting our terms!");
+  // You can also display a thank you message or perform other actions
+});
---
+++ thank-you.js
@@ -0,0 +1,6 @@
+// Function for accepting
+document.querySelector('.dismiss-btn').addEventListener('click', function() {
+    alert("Thank you for accepting our terms!");
+    // You can also display a thank you message or perform other actions
+  });
+  
